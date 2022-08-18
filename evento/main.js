80c7b9003827669ad32586d7124044e3f6b9609c
luisbboca <107447827+luisbboca@users.noreply.github.com>
2022-08-17 23:55:42 -0300

    Add files via upload

---
+++ evento/main.js
@@ -0,0 +1,24 @@
+//azul
+(() =>{
+    let btnAzul = document.getElementById('btnAzul');
+
+    btnAzul.addEventListener('click', () => {
+        document.body.style.backgroundColor = 'blue';
+    }, false);
+})();
+//rojo
+(() =>{
+    let btnRojo = document.getElementById('btnRojo');
+
+    btnRojo.addEventListener('click', () => {
+        document.body.style.backgroundColor = 'red';
+    }, false);
+})();
+//amarillo
+(() =>{
+    let btnAmarillo = document.getElementById('btnAmarillo');
+
+    btnAmarillo.addEventListener('click', () => {
+        document.body.style.backgroundColor = 'yellow';
+    }, false);
+})();
